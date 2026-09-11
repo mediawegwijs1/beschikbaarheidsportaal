b821c0b5eec520db4e038b30ed55e8cb94b0262e
mediawegwijs1 <office@mediawegwijs.nl>
2026-09-11 15:58:18 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // SERVICE WORKER V2.1 (Instant Cache-First & Auto-Activation)
-const CACHE_NAME = 'mediawegwijs-v2.4';
+const CACHE_NAME = 'mediawegwijs-v2.5';
 const STATIC_ASSETS = [
   './',
   './index.html',
